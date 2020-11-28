8be837310daaccd68dd3de608191003e694a2616
cTraymany <chindalatht@gmail.com>
2020-11-28 13:38:31 -0800

    namespaces routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  resources :contacts
-  resources :groups
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :contacts
+      resources :groups
+    end
+  end
 end
