3914a9d180d0ad7e2821a624222d0c5d0f8e8b3b
cTraymany <chindalatht@gmail.com>
2020-11-29 08:08:43 -0800

    adds index and create actions for groups

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -1,2 +1,22 @@
 class Api::V1::GroupsController < ApplicationController
+    def index
+        render json: Group.all
+    end
+
+    def create
+        group = Group.new(group_params)
+        if group.save
+            render json: group
+        else
+            render json: {message: "Please try again."}
+        end
+    end
+
+
+
+    private
+
+    def group_params
+        params.require(:group).permit(:name)
+    end
 end
