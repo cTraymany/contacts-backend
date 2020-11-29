3a76619b9b5ae2425ba3ec81b66708948e31f2ce
cTraymany <chindalatht@gmail.com>
2020-11-29 08:10:11 -0800

    adds index action for contacts

--- app/controllers/api/v1/contacts_controller.rb
+++ app/controllers/api/v1/contacts_controller.rb
@@ -1,2 +1,30 @@
 class Api::V1::ContactsController < ApplicationController
+    before_action :set_group
+    
+    def index
+        render json: group.contacts
+        binding.pry
+    end
+
+    def create
+
+    end
+
+    def show
+
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def set_group
+        group = Group.find(params[:group_id])
+    end
+
+    def contact_params
+        params.require(:contact).permit(:name, :phoneNumber, :birthday, :group_id)
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,5 @@ dad = Contact.create(name: "dad", group_id:1, phoneNumber: 1123456789, birthday:
 
 # group_id: 2
 friends = Group.create(name: "Friends")
+
 chromeBoy = Contact.create(name: "Chrome Boy", group_id: 2, phoneNumber: 1010101010, birthday: Time.new(2020, 2, 2).strftime("%m/%d/%Y"))
